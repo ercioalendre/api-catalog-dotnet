90105dcab626347b263860dec9a60b13b68ce0dc
Ercio Alendre <ercio.alendre@sempreit.com.br>
2023-08-01 00:32:42 -0300

    FIX: corrige implementação de autorização na documentação Swagger

--- Program.cs
+++ Program.cs
@@ -24,26 +24,33 @@ namespace APICatalog
 
             builder.Services.AddSwaggerGen(options =>
             {
-                options.SwaggerDoc(
-                    "v1",
-                    new OpenApiInfo { 
-                        Title = "API Catalog",
-                        Version = "v1"
-                    }
-                );
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "apiagenda", Version = "v1" });
 
-                options.AddSecurityDefinition(
-                    "Bearer",
-                    new OpenApiSecurityScheme()
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Header de autorização JWT usando o esquema Bearer.\r\n\r\nInforme 'Bearer'[espaço] e o seu token.\r\n\r\nExamplo: \'Bearer 12345abcdef\'",
+                });
+
+                options.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement 
                     {
-                        Name = "Authorization",
-                        Type = SecuritySchemeType.ApiKey,
-                        Scheme = "Bearer",
-                        BearerFormat = "JWT",
-                        In = ParameterLocation.Header,
-                        Description = "Header de autorização JWT usando o esquema Bearer.\r\n\r\nInforme 'Bearer'[espaço] e o seu token.\r\n\r\nExamplo: \'Bearer 12345abcdef\'",
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                        Array.Empty<string>()
                     }
-                );
+                });
             });
 
             string? databaseConnection = builder.Configuration.GetConnectionString("DefaultConnection");
